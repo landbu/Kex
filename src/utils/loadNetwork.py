87bb14dcf05ed427d5e09a0076ba58def9a516c8
Samuel Fors <safor@kth.se>
2026-04-13 21:45:10 +0200

    Updates test and loadNetwork

--- src/utils/loadNetwork.py
+++ src/utils/loadNetwork.py
@@ -4,7 +4,8 @@ import torch.nn as nn
 from architectures.networkArchitectures import networkRegistry
 
 def loadNetwork(name: str) -> nn.Module:
-    NetworkClass = networkRegistry[name]
+    NetworkEntry = networkRegistry[name]
+    NetworkClass = NetworkEntry.NetworkClass
     network = NetworkClass()
     network.load_state_dict(state_dict = torch.load(f"networks/{name}.pth"))
     network.eval()
